98a887278db2e9c6fa3dddc8c65e5571edde25e4
Tim Loenko <tima.loenko@gmail.com>
2022-08-27 15:36:08 +0500

    update about and first post

--- src/config.cjs
+++ src/config.cjs
@@ -1,15 +1,15 @@
 export default {
   siteBaseUrl: '/',
-  siteLanguage: "en",
-  siteTitle: "Astro Themes",
-  siteDescription: "<your site description>",
-  siteCopyright: "© 2022 copyright text.",
+  siteLanguage: "ru",
+  siteTitle: "Творчество Тимофея Роняева",
+  siteDescription: "Стихотворения, рассказы, эпиграммы и т.п.",
+  siteCopyright: "© 2022 Timofey Ronyaev",
   dateFormat: "MMMM D, YYYY",
   paginate: 6,
   memus:[
     {
       identifier: "about",
-      name: "About",
+      name: "Об авторе",
       url: "/about/",
     }
   ],
