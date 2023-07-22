eb40937696cab32fac6354829365bfc379d266b0
tima-loenko <tima.loenko@gmail.com>
2023-07-22 13:46:15 +0500

    some updates, some fixes

--- src/config.cjs
+++ src/config.cjs
@@ -3,8 +3,8 @@ export default {
   siteLanguage: "ru",
   siteTitle: "Творчество Тимофея Роняева",
   siteDescription: "Стихотворения, рассказы, эпиграммы и т.п.",
-  siteCopyright: "© 2022 Timofey Ronyaev",
-  dateFormat: "MMMM D, YYYY",
+  siteCopyright: "© 2023 Timofey Ronyaev",
+  dateFormat: "DD/MM/YYYY",
   paginate: 6,
   memus:[
     {
--- src/pages/posts/alexander-from-nefteugansk.md
+++ src/pages/posts/alexander-from-nefteugansk.md
@@ -2,7 +2,7 @@
 title: "Эпиграмма"
 date: 2022-08-29T00:00:00Z
 slug: "alexander-from-nefteugansk"
-description: "На Александра из Нефтеуганска"
+description: "На Александра из Нефтеюганска"
 categories: ["стихи"]
 tags: ["эпиграммы"]
 layout: "../../layouts/PostLayout.astro"
